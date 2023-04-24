3fa67fcb3745f3388b6764baa1077a69fdb549be
Finn Heemeyer <finn.heemeyer@comgy.io>
2023-04-24 11:35:00 +0200

    Disable ghost text
    It was interfering with gh copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -87,11 +87,6 @@ return {
             return item
           end,
         },
-        experimental = {
-          ghost_text = {
-            hl_group = "LspCodeLens",
-          },
-        },
       }
     end,
   },
