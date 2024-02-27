3a11292108b30837d6d4ac7372240449951d1cf0
Finn Heemeyer <github@tonekk.de>
2024-02-27 15:38:05 +0100

    Add minimal vim config to use with vscode

---
+++ lua/minimal/setup.lua
@@ -0,0 +1,28 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+})
+
+vim.opt.swapfile = false
+vim.opt.backup = false
