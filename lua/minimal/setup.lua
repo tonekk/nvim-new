616005b1d4e4e7ef6fa0ff1fc4905dd5920b2ae3
Finn Heemeyer <github@tonekk.de>
2024-03-05 16:39:17 +0100

    Add substitute.nvim to vscode-nvim

--- lua/minimal/setup.lua
+++ lua/minimal/setup.lua
@@ -17,12 +17,19 @@ require("lazy").setup({
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
+      require("nvim-surround").setup({})
     end,
   },
+  {
+    "gbprod/substitute.nvim",
+    opts = {},
+  },
 })
+
+vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
+vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
+vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
+vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
 
 vim.opt.swapfile = false
 vim.opt.backup = false
