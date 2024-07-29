a590598a459315b8331c3662d7a2d288a2c3f67a
Finn Heemeyer <github@tonekk.de>
2024-07-29 18:01:29 +0200

    Bye copilot
    AI hype is over

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -193,28 +193,6 @@ return {
     end,
   },
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<C-a>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-        },
-      })
-    end,
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       indent = {
